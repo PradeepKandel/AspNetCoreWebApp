ae332c62287be9d0a6b301046eb596543a76a229
Pradeep Kandel <pradeep_kandel@hotmail.com>
2023-04-13 16:12:04 +0800

    endpoint and subscriptionkey updated.

--- AspNetCoreWebApp/Program.cs
+++ AspNetCoreWebApp/Program.cs
@@ -1,4 +1,23 @@
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+var endpoint = "https://computervision-merocloud.cognitiveservices.azure.com/";
+var subscriptionKey = "db355c27a1504e75918e64b30b737471";
+
+var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey))
+{
+    Endpoint = endpoint
+};
 var builder = WebApplication.CreateBuilder(args);
+// Step 9- Use the AnalyzeImageAsync method to analyze an image using the API. For example, the following code analyzes an image stored in a byte array:
+var imageBytes = File.ReadAllBytes("path/to/image.jpg");
+var features = new List<VisualFeatureTypes>
+{
+    VisualFeatureTypes.Categories, VisualFeatureTypes.Description, VisualFeatureTypes.Tags
+};
+
+var result = await client.AnalyzeImageInStreamAsync(new MemoryStream(imageBytes), features);
+// Step 9 ends
 
 // Add services to the container.
 builder.Services.AddRazorPages();
